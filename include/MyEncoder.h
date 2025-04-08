8720a851d536982ddaf43699c02976dabd2fdc79
vudinhso <vudinhso@gmail.com>
2025-04-08 09:16:42 +0700

    Fix typos in MyEncoder.h comments and update library integration note

--- include/MyEncoder.h
+++ include/MyEncoder.h
@@ -2,7 +2,7 @@
 #define MY_ENCODER_H
 
 #include <Arduino.h>      // Include the Arduino core library
-#include <ESP32Encoder.h> // https://github.com/madhephaestus/ESP32Encoder.git
+#include <ESP32Encoder.h> // Look at the paltformIO.ici to integrate external libraries: https://github.com/madhephaestus/ESP32Encoder.git
 
 class Encoder
 {
@@ -12,7 +12,7 @@ public:
     long getCount();               // Method to get the current count of the encoder
 
 private:
-    byte _ENCA, _ENCB;     // Locla variable for the ENCA and ENCB
+    byte _ENCA, _ENCB;     // Local variable for the ENCA and ENCB
     ESP32Encoder _encoder; // Instance of the ESP32Encoder class to handle encoder functionality
 };
 
